91f53802f164284faad51d931b2ee359039fb4b5
Ahmad <root@ahmadafuni.com>
2025-07-07 11:23:44 +1000

    Additional error reporting for custom predicates

--- src/middleware/error.rs
+++ src/middleware/error.rs
@@ -2,7 +2,9 @@
 
 use std::{backtrace::Backtrace, fmt::Debug};
 
-use crate::middleware::{Operation, Statement, StatementArg};
+use crate::middleware::{
+    CustomPredicate, Key, Operation, Statement, StatementArg, StatementTmplArg, Value, Wildcard,
+};
 
 pub type Result<T, E = Error> = core::result::Result<T, E>;
 
@@ -18,6 +20,18 @@ pub enum MiddlewareInnerError {
     MaxLength(String, usize, usize),
     #[error("{0} amount of {1} should be {1} but it's {2}")]
     DiffAmount(String, String, usize, usize),
+    #[error("{0} should be assigned the value {1} but has previously been assigned {2}")]
+    InvalidWildcardAssignment(Wildcard, Value, Value),
+    #[error("{0} does not match {1}")]
+    MismatchedAnchoredKeyInStatementTmplArg(Key, Key),
+    #[error("{0} does not match against {1}")]
+    MismatchedStatementTmplArg(StatementTmplArg, StatementArg),
+    #[error("{0} does not match {1}")]
+    MismatchedWildcardValueAndStatementArg(Value, Value),
+    #[error("Not all statement templates of {0} have been matched")]
+    UnsatisfiedCustomPredicateConjunction(CustomPredicate),
+    #[error("None of the statement templates of {0} have been matched")]
+    UnsatisfiedCustomPredicateDisjunction(CustomPredicate),
     // Other
     #[error("{0}")]
     Custom(String),
@@ -64,6 +78,34 @@ impl Error {
     }
     pub(crate) fn diff_amount(obj: String, unit: String, expect: usize, found: usize) -> Self {
         new!(DiffAmount(obj, unit, expect, found))
+    }
+    pub(crate) fn invalid_wildcard_assignment(
+        wildcard: Wildcard,
+        value: Value,
+        prev_value: Value,
+    ) -> Self {
+        new!(InvalidWildcardAssignment(wildcard, value, prev_value))
+    }
+    pub(crate) fn mismatched_anchored_key_in_statement_tmpl_arg(key_tmpl: Key, key: Key) -> Self {
+        new!(MismatchedAnchoredKeyInStatementTmplArg(key_tmpl, key))
+    }
+    pub(crate) fn mismatched_statement_tmpl_arg(
+        st_tmpl_arg: StatementTmplArg,
+        st_arg: StatementArg,
+    ) -> Self {
+        new!(MismatchedStatementTmplArg(st_tmpl_arg, st_arg))
+    }
+    pub(crate) fn mismatched_wildcard_value_and_statement_arg(
+        wc_value: Value,
+        st_arg: Value,
+    ) -> Self {
+        new!(MismatchedWildcardValueAndStatementArg(wc_value, st_arg))
+    }
+    pub(crate) fn unsatisfied_custom_predicate_conjunction(pred: CustomPredicate) -> Self {
+        new!(UnsatisfiedCustomPredicateConjunction(pred))
+    }
+    pub(crate) fn unsatisfied_custom_predicate_disjunction(pred: CustomPredicate) -> Self {
+        new!(UnsatisfiedCustomPredicateDisjunction(pred))
     }
     pub(crate) fn custom(s: String) -> Self {
         new!(Custom(s))
