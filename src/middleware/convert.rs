d733f2f0833184c6afbd37b5df311b3457f41df4
Daniel Gulotta <dgulotta@alum.mit.edu>
2025-09-14 12:26:10 -0700

    allow conversion to Dictionary etc

--- src/middleware/convert.rs
+++ src/middleware/convert.rs
@@ -58,60 +58,168 @@ where
     }
 }
 
-pub trait TryIntoValue {
+pub trait PodTryInto<T> {
     type Error;
-    fn try_into_value(self, params: &Params) -> std::result::Result<Value, Self::Error>;
+    fn pod_try_into(self, params: &Params) -> std::result::Result<T, Self::Error>;
 }
 
-pub fn try_into_array<C>(c: C, params: &Params) -> Result<Value, Error>
+pub fn try_into_array<C>(c: C, params: &Params) -> Result<Array, Error>
 where
     C: IntoIterator,
-    C::Item: TryIntoValue,
-    Error: From<<C::Item as TryIntoValue>::Error>,
+    C::Item: PodTryInto<Value>,
+    Error: From<<C::Item as PodTryInto<Value>>::Error>,
 {
-    let r: Result<Vec<Value>, <C::Item as TryIntoValue>::Error> =
-        c.into_iter().map(|x| x.try_into_value(params)).collect();
-    let arr = Array::new(params.max_depth_mt_containers, r?)?;
-    Ok(Value::from(arr))
+    let r: Result<Vec<Value>, <C::Item as PodTryInto<Value>>::Error> =
+        c.into_iter().map(|x| x.pod_try_into(params)).collect();
+    Ok(Array::new(params.max_depth_mt_containers, r?)?)
 }
 
-pub fn try_into_set<C>(c: C, params: &Params) -> Result<Value, Error>
+pub fn try_into_array_value<C>(c: C, params: &Params) -> Result<Value, Error>
 where
     C: IntoIterator,
-    C::Item: TryIntoValue,
-    Error: From<<C::Item as TryIntoValue>::Error>,
+    C::Item: PodTryInto<Value>,
+    Error: From<<C::Item as PodTryInto<Value>>::Error>,
 {
-    let r: Result<HashSet<Value>, <C::Item as TryIntoValue>::Error> =
-        c.into_iter().map(|x| x.try_into_value(params)).collect();
-    let set = Set::new(params.max_depth_mt_containers, r?)?;
-    Ok(Value::from(set))
+    Ok(Value::from(try_into_array(c, params)?))
 }
 
-pub fn try_into_dict<C, K, V>(c: C, params: &Params) -> Result<Value, Error>
+pub fn try_into_set<C>(c: C, params: &Params) -> Result<Set, Error>
+where
+    C: IntoIterator,
+    C::Item: PodTryInto<Value>,
+    Error: From<<C::Item as PodTryInto<Value>>::Error>,
+{
+    let r: Result<HashSet<Value>, <C::Item as PodTryInto<Value>>::Error> =
+        c.into_iter().map(|x| x.pod_try_into(params)).collect();
+    Ok(Set::new(params.max_depth_mt_containers, r?)?)
+}
+
+pub fn try_into_set_value<C>(c: C, params: &Params) -> Result<Value, Error>
+where
+    C: IntoIterator,
+    C::Item: PodTryInto<Value>,
+    Error: From<<C::Item as PodTryInto<Value>>::Error>,
+{
+    Ok(Value::from(try_into_set(c, params)?))
+}
+
+pub fn try_into_dict<C, K, V>(c: C, params: &Params) -> Result<Dictionary, Error>
 where
     C: IntoIterator<Item = (K, V)>,
     K: Into<String>,
-    V: TryIntoValue,
+    V: PodTryInto<Value>,
     Error: From<V::Error>,
 {
     let r: Result<HashMap<Key, Value>, V::Error> = c
         .into_iter()
-        .map(|(k, v)| Ok((Key::from(k.into()), v.try_into_value(params)?)))
+        .map(|(k, v)| Ok((Key::from(k.into()), v.pod_try_into(params)?)))
         .collect();
-    let dict = Dictionary::new(params.max_depth_mt_containers, r?)?;
-    Ok(Value::from(dict))
+    Ok(Dictionary::new(params.max_depth_mt_containers, r?)?)
+}
+
+pub fn try_into_dict_value<C, K, V>(c: C, params: &Params) -> Result<Value, Error>
+where
+    C: IntoIterator<Item = (K, V)>,
+    K: Into<String>,
+    V: PodTryInto<Value>,
+    Error: From<V::Error>,
+{
+    Ok(Value::from(try_into_dict(c, params)?))
 }
 
 #[macro_export]
 macro_rules! try_into_using_value_from {
     ($t: ty) => {
-        impl $crate::middleware::convert::TryIntoValue for $t {
+        impl $crate::middleware::convert::PodTryInto<$crate::middleware::Value> for $t {
             type Error = core::convert::Infallible;
-            fn try_into_value(
+            fn pod_try_into(
                 self,
                 _params: &$crate::middleware::Params,
-            ) -> std::result::Result<$crate::middleware::Value, Self::Error> {
+            ) -> core::result::Result<$crate::middleware::Value, Self::Error> {
                 Ok($crate::middleware::Value::from(self))
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! try_into_using_array {
+     ($($path:ident)::+ ) => {
+        impl<__T> $crate::middleware::convert::PodTryInto<$crate::middleware::containers::Array> for$($path)::+<__T>
+        where
+            __T: $crate::middleware::convert::PodTryInto<$crate::middleware::Value>,
+            $crate::middleware::Error: core::convert::From<__T::Error>,
+        {
+            type Error = $crate::middleware::Error;
+            fn pod_try_into(self, params: &$crate::middleware::Params) -> core::result::Result<$crate::middleware::containers::Array, Self::Error> {
+                $crate::middleware::convert::try_into_array(self, params)
+            }
+        }
+
+        impl<__T> $crate::middleware::convert::PodTryInto<$crate::middleware::Value> for$($path)::+<__T>
+        where
+            __T: $crate::middleware::convert::PodTryInto<$crate::middleware::Value>,
+            $crate::middleware::Error: core::convert::From<__T::Error>,
+        {
+            type Error = $crate::middleware::Error;
+            fn pod_try_into(self, params: &$crate::middleware::Params) -> core::result::Result<$crate::middleware::Value, Self::Error> {
+                $crate::middleware::convert::try_into_array_value(self, params)
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! try_into_using_set {
+     ($($path:ident)::+ ) => {
+        impl<__T> $crate::middleware::convert::PodTryInto<$crate::middleware::containers::Set> for$($path)::+<__T>
+        where
+            __T: $crate::middleware::convert::PodTryInto<$crate::middleware::Value>,
+            $crate::middleware::Error: core::convert::From<__T::Error>,
+        {
+            type Error = $crate::middleware::Error;
+            fn pod_try_into(self, params: &$crate::middleware::Params) -> core::result::Result<$crate::middleware::containers::Set, Self::Error> {
+                $crate::middleware::convert::try_into_set(self, params)
+            }
+        }
+
+        impl<__T> $crate::middleware::convert::PodTryInto<$crate::middleware::Value> for$($path)::+<__T>
+        where
+            __T: $crate::middleware::convert::PodTryInto<$crate::middleware::Value>,
+            $crate::middleware::Error: core::convert::From<__T::Error>,
+        {
+            type Error = $crate::middleware::Error;
+            fn pod_try_into(self, params: &$crate::middleware::Params) -> core::result::Result<$crate::middleware::Value, Self::Error> {
+                $crate::middleware::convert::try_into_set_value(self, params)
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! try_into_using_dict {
+     ($($path:ident)::+ ) => {
+        impl<__K, __V> $crate::middleware::convert::PodTryInto<$crate::middleware::containers::Dictionary> for$($path)::+<__K, __V>
+        where
+            __K: core::convert::Into<std::string::String>,
+            __V: $crate::middleware::convert::PodTryInto<$crate::middleware::Value>,
+            $crate::middleware::Error: core::convert::From<__V::Error>,
+        {
+            type Error = $crate::middleware::Error;
+            fn pod_try_into(self, params: &$crate::middleware::Params) -> core::result::Result<$crate::middleware::containers::Dictionary, Self::Error> {
+                $crate::middleware::convert::try_into_dict(self, params)
+            }
+        }
+
+        impl<__K, __V> $crate::middleware::convert::PodTryInto<$crate::middleware::Value> for$($path)::+<__K, __V>
+        where
+            __K: core::convert::Into<std::string::String>,
+            __V: $crate::middleware::convert::PodTryInto<$crate::middleware::Value>,
+            $crate::middleware::Error: core::convert::From<__V::Error>,
+        {
+            type Error = $crate::middleware::Error;
+            fn pod_try_into(self, params: &$crate::middleware::Params) -> core::result::Result<$crate::middleware::Value, Self::Error> {
+                $crate::middleware::convert::try_into_dict_value(self, params)
             }
         }
     };
@@ -127,59 +235,8 @@ try_into_using_value_from!(Set);
 try_into_using_value_from!(SecretKey);
 try_into_using_value_from!(String);
 
-impl<T> TryIntoValue for Vec<T>
-where
-    T: TryIntoValue,
-    Error: From<T::Error>,
-{
-    type Error = Error;
-    fn try_into_value(self, params: &Params) -> std::result::Result<Value, Self::Error> {
-        try_into_array(self, params)
-    }
-}
-
-impl<T> TryIntoValue for HashSet<T>
-where
-    T: TryIntoValue,
-    Error: From<T::Error>,
-{
-    type Error = Error;
-    fn try_into_value(self, params: &Params) -> std::result::Result<Value, Self::Error> {
-        try_into_set(self, params)
-    }
-}
-
-impl<T> TryIntoValue for std::collections::BTreeSet<T>
-where
-    T: TryIntoValue,
-    Error: From<T::Error>,
-{
-    type Error = Error;
-    fn try_into_value(self, params: &Params) -> std::result::Result<Value, Self::Error> {
-        try_into_set(self, params)
-    }
-}
-
-impl<K, V> TryIntoValue for HashMap<K, V>
-where
-    String: From<K>,
-    V: TryIntoValue,
-    Error: From<V::Error>,
-{
-    type Error = Error;
-    fn try_into_value(self, params: &Params) -> std::result::Result<Value, Self::Error> {
-        try_into_dict(self, params)
-    }
-}
-
-impl<K, V> TryIntoValue for std::collections::BTreeMap<K, V>
-where
-    String: From<K>,
-    V: TryIntoValue,
-    Error: From<V::Error>,
-{
-    type Error = Error;
-    fn try_into_value(self, params: &Params) -> std::result::Result<Value, Self::Error> {
-        try_into_dict(self, params)
-    }
-}
+try_into_using_array!(Vec);
+try_into_using_set!(HashSet);
+try_into_using_set!(std::collections::BTreeSet);
+try_into_using_dict!(HashMap);
+try_into_using_dict!(std::collections::BTreeMap);
