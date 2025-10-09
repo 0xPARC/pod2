9a45f56bf5c789cb2df85b6ca3f7ea85f563f2f9
Rob Knight <mail@robknight.org.uk>
2025-10-09 09:20:56 +0200

    New 'use' syntax with support for intro predicates

--- src/lang/mod.rs
+++ src/lang/mod.rs
@@ -687,7 +687,7 @@ mod tests {
         let batch_id_str = available_batch.id().encode_hex::<String>();
         let input = format!(
             r#"
-            use imported_pred from 0x{}
+            use batch imported_pred from 0x{}
 
             REQUEST(
                 imported_pred(Pod1, Pod2)
@@ -738,7 +738,7 @@ mod tests {
 
         let input = format!(
             r#"
-            use pred_one, _, pred_three from 0x{}
+            use batch pred_one, _, pred_three from 0x{}
 
             REQUEST(
                 pred_one(Pod1)
@@ -796,7 +796,7 @@ mod tests {
 
         let input = format!(
             r#"
-            use imported_eq from 0x{}
+            use batch imported_eq from 0x{}
 
             wrapper_pred(X, Y) = AND(
                 imported_eq(X, Y)
@@ -832,6 +832,36 @@ mod tests {
         };
 
         assert_eq!(defined_pred.statements[0], expected_statement);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_e2e_intro_import_parsing() -> Result<(), LangError> {
+        let params = Params::default();
+
+        let intro_hash = "0x".to_string() + &"a".repeat(64);
+        let input = format!(
+            r#"
+            use intro IntroStmt(a1, a2) from {}
+
+            REQUEST(
+                IntroStmt(X, Y)
+            )
+        "#,
+            intro_hash
+        );
+
+        let processed = parse(&input, &params, &[])?;
+        let request_templates = processed.request.templates();
+        assert_eq!(request_templates.len(), 1);
+
+        if let Predicate::Intro(intro_ref) = &request_templates[0].pred {
+            assert_eq!(intro_ref.name, "IntroStmt");
+            assert_eq!(intro_ref.args_len, 2);
+        } else {
+            panic!("Expected Intro predicate");
+        }
 
         Ok(())
     }
@@ -920,7 +950,7 @@ mod tests {
 
         let input = format!(
             r#"
-            use some_pred from {}
+            use batch some_pred from {}
             "#,
             unknown_batch_id
         );
